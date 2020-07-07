043bed1a68c08c8a04bc56c43d8c87070f7c14c5
Bruno Chirelli <brunochirelli@gmail.com>
2020-07-07 12:06:38 -0300

    add new layout for checkout dialog

--- src/components/form/Confirm.js
+++ src/components/form/Confirm.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Button } from 'gatsby-theme-material-ui';
-import { Dialog, Card, CardContent, Typography, CardActions } from '@material-ui/core';
+import { Dialog, Typography, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
 import moment from 'moment';
 
 const RowStyled = styled.div`
@@ -25,33 +26,82 @@ const DialogContentStyled = styled(DialogContent)`
       product.toLowerCase() === 'integral' ? theme.palette.primary.main : theme.palette.secondary.main};
   }
 `;
+
 const Confirm = ({ values, submit, setConfirmShow, confirmShow, setSubmitting }) => {
+  const theme = useTheme();
 
   const handleClose = () => {
     setConfirmShow(false);
     setSubmitting(false);
   };
   return (
+    <Dialog onClose={handleClose} open={confirmShow} scroll="body" maxWidth="sm" fullWidth>
+      <DialogTitle component="header">
+        <Typography variant="h5">Check your order</Typography>
+      </DialogTitle>
+
+      <DialogContentStyled theme={theme} product={values.product}>
+        <Typography className="product-color" variant="h6">
+          {values.product}
+        </Typography>
+        <Typography className="product-color" style={{ marginBottom: '1rem' }}>
+          Product description
+        </Typography>
+
+        <Typography variant="h6" gutterBottom>
+          {values.name}
+        </Typography>
+
         <RowStyled>
           <Typography variant="body2">Event date:</Typography>
-          {values.street ? (
-            <Typography>
-              Endereço: {values.street}, {values.number} - {values.neighbor}
+          <Typography variant="body1">{values.date.format('L')}</Typography>
+        </RowStyled>
+        <RowStyled>
+          <Typography variant="body2">Quantity:</Typography>
+          <Typography variant="body1">{values.quantity}</Typography>
+        </RowStyled>
+        <RowStyled>
+          <Typography variant="body2">Contact phone:</Typography>
+          <Typography variant="body1">{values.phone}</Typography>
+        </RowStyled>
+
+        {values.street ? (
+          <>
+            <Typography variant="h6" gutterBottom>
+              Address
             </Typography>
-          ) : (
-            <Typography>Retirada</Typography>
-          )}
-          <Typography>Data do evento: {values.date.format('L')}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="contained" color="primary" onClick={submit}>
-            Confirmar pedido
-          </Button>
-          <Button variant="outlined" color="secondary" onClick={handleClose}>
-            Alterar
-          </Button>
-        </CardActions>
-      </Card>
+            <Typography>
+              {values.street} {values.number} - {values.neighbor}
+            </Typography>
+          </>
+        ) : (
+          <>
+            <Typography variant="h6" gutterBottom>
+              Pick up in store
+            </Typography>
+            <Typography>You can pick up in the nearest store.</Typography>
+          </>
+        )}
+      </DialogContentStyled>
+
+      <DialogActions>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          disableElevation
+          onClick={submit}
+          style={{
+            flexGrow: 1,
+            background: `linear-gradient(90deg, ${theme.palette.primary.main},${theme.palette.secondary.main})`,
+          }}
+        >
+          Order now
+        </Button>
+        <Button variant="outlined" color="secondary" size="large" onClick={handleClose}>
+          Change
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
